32a3daf1bad99d15203b25cf4bbb81cd5f312fd0
veloz-junior <seu@email.com>
2026-08-30 14:08:50 -0400

    refac(personal): v1.8.0  replace email with shareable tag when adding student

--- src/main/java/com/TechFit/TechFit/TechFitApplication.java
+++ src/main/java/com/TechFit/TechFit/TechFitApplication.java
@@ -2,8 +2,11 @@ package com.TechFit.TechFit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+
 public class TechFitApplication {
 
 	public static void main(String[] args) {
